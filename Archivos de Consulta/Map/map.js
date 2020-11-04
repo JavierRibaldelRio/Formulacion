e09341a7a2f244631bade3b1364dfcd6745cd7b8
JavierRibaldelRio <javierribal@gmail.com>
2020-11-04 23:09:10 +0100

    He añadido un forEach al map de ejemplo

--- Archivos de Consulta/Map/map.js
+++ Archivos de Consulta/Map/map.js
@@ -1,17 +1,18 @@
-
-var mapaNuevo = new Map(); 	//Definir el map
+var mapaNuevo = new Map(); //Definir el map
 
 
 //añadir cosas al map
 
-mapaNuevo.set("Oxígen",2);
+mapaNuevo.set("Oxígen", 2);
 
-mapaNuevo.set("Hidrogen",2);
+mapaNuevo.set("Hidrogen", 2);
 
 //Sacar cosas del map por clave
 
 alert(mapaNuevo.get("Oxígen"));
 
+mapaNuevo.forEach(function(valor, clave) {
 
+    alert(valor + clave);
 
-
+})
