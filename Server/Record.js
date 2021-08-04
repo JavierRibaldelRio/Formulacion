02bd74c04f90207ae91cc8a3c117ec4801483e7c
JavierRibaldelRio <javierribal@gmail.com>
2021-08-04 23:24:13 +0200

    He avanzado

---
+++ Server/Record.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'); //Importa la libreria de Mongo
+
+mongoose.connect("mongodb+srv://Javier:Midnareal0@cluster0.x1t0q.mongodb.net/formulacion?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+var Schema = mongoose.Schema;
+
+var recordsEsquema = new Schema({
+    nick: { type: String, required: true, uniqued: true },
+
+    puntos: { type: Number, required: true }
+})
+
+module.exports = mongoose.model('Record', recordsEsquema);
---
+++ Server/index.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var app = express();
+
+var Record = require("./Record.js");
+//Utiliza ejs
+app.set('view engine', 'ejs');
+
+//boddy parse
+
+app.use(express.urlencoded({ extended: true }));
+
+//Para usar la parte estatica
+
+app.use("/comprobar", (req, res) => {
+
+    let nick = req.body.nick;       //almacena el nombre
+
+    let puntos = req.body.puntos;   //puntos
+
+    var nuevoRecord = new Record({ nick: nick, puntos: puntos });  //Cre el objeto de record
+
+    console.log(nuevoRecord);
+
+    //Busca a todas las personas
+    Record.find({}, (err, todosLosRecords) => {
+
+
+
+        //Si se ha producido un error
+        if (err) {
+
+            res.type("html").status(500).send(`Se a producido un error: ${err}`);
+        } else {
+
+
+            //Ordena por el numero
+            todosLosRecords.sort((a, b) => {
+
+                return a.puntos - b.puntos
+            })
+
+            console.log(todosLosRecords);
+
+            //Comprueba si el nuvo record supera al ultimo
+
+            if (todosLosRecords[0].puntos < nuevoRecord.puntos) {   //Si los puntos del usuario son mayores que el record
+
+                //Elimina el ultimo
+                Record.findOneAndRemove({ nick: todosLosRecords[0].nick }, (err) => {
+                    if (err) {
+                        res.type("html").status("500").send(`Se ha producido un error: ${err}`)
+                    } else {
+
+
+
+                        //Y guarda los datos del usuario
+                        nuevoRecord.save((err) => {
+
+                            if (err) {
+                                res.type("html").send("Se a cometido un error" + err).status(500);
+                            }
+
+                            else {
+
+                                res.status(200).type("html").send(`Se ha registrado los ${puntos} puntos de ${nick}`);
+                            }
+                        });
+
+
+                    }
+                });
+
+
+
+            }
+
+            else {
+
+                res.type("html").status(200).send("No has entrado en la tabla de calsificacion")
+            }
+
+
+        }
+    }).sort({ 'puntos': 1 });     //ordena por puntuacion
+
+
+});
+
+app.use('/public', express.static('public'));
+
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
